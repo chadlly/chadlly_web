95af229fb8f1c85ed44fb4d61c6d1543f31a1910
xmapple <mjkgrc@gmail.com>
2020-12-01 09:46:57 +0900

    implement mongodb config

--- server/routes/maplogic/mapservice.js
+++ server/routes/maplogic/mapservice.js
@@ -1,7 +1,58 @@
-var Location = require("../../model/location");
+const Location = require("../../model/location");
+const User = require("../../model/user");
+
+var MongoClient = require("mongodb").MongoClient;
+// db collections variable
+let database;
+var userCollection;
+var categoryCollection;
+let campingCollection;
+let fishingCollection;
+let forestCollection;
+let museumCollection;
+let parkingCollection;
+let roadCollection;
+let ruinsCollection;
+let siteCollection;
+let valleyCollection;
+// end
+// db connect code
+
+function connectdb(){
+    let databaseUrl = "mongodb+srv://Chaduri:heychaduri@cluster0.ggtbm.mongodb.net/db?retryWrites=true&w=majority";
+    MongoClient.connect(databaseUrl, {useNewUrlParser: true, useUnifiedTopology: true}, function(err,db){
+    if(err){
+        console.log(' database error !');
+        return; 
+    }
+    
+    console.log('DB:: connect to ' + databaseUrl);
+    database = db.db('db');
+    userCollection = database.collection("user");
+    categoryCollection = database.collection("category");
+    campingCollection = database.collection("camping");
+    fishingCollection = database.collection("fishing");
+    forestCollection = database.collection("forest");
+    museumCollection = database.collection("museum");
+    parkingCollection = database.collection("parking");
+    roadCollection = database.collection("road");
+    ruinsCollection = database.collection("ruins");
+    siteCollection = database.collection("site");
+    valleyCollection = database.collection("valley");
+    
+    console.log("db connected!");
+    
+    findLocations();
+    });
+}
+
+connectdb();
 
 
-var locations = [];
+let location;
+
+let locations = [];
+let travelLocations;
 
 function initTest(){
 
@@ -27,6 +78,41 @@ function printLocations(){
 function getLocations(){
     return locations;
 }
+
+function findLocations(){
+
+    categoryCollection.find({}).toArray(function(err, result){
+        if(err){
+            callback("dbOpenError", "none");
+            throw err;
+        }
+        if(result.length > 0){
+            //console.log(result);
+            //callback("", result);
+
+            travelLocations = result;
+            console.log(travelLocations);
+        }else{
+            console.log('no entry in db');
+            callback("error", "none");
+        }
+    })
+}
+
+function getNearestPlaces(lat, lng, callback){
+    
+
+}
+
+
+
+function recommend(){
+    
+}
+
+
+
+
 
 
 
